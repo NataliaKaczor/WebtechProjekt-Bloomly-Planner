ea97c965a644ebfbea7bdb5279e081d013df33b3
Natalia Kaczor <Natalia.Kaczor@Student.HTW-Berlin.de>
2026-06-13 14:25:51 +0200

    Startseite Countdown JS Funktion fertig

--- frontend/src/app/dashboard/dashboard.ts
+++ frontend/src/app/dashboard/dashboard.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
-export class Dashboard {
+export class Dashboard implements OnInit {
 
+  countdown: string = '';
+  tage: number = 0;
+  stunden: number = 0;
+  minuten: number = 0;
+  sekunden: number = 0;
+  partyStarted: boolean = false; 
+
+  ngOnInit(): void {
+
+    const partyDatum = new Date('2026-08-05T10:00:00').getTime();
+
+    setInterval(() => {
+
+      const aktuelleZeit = new Date().getTime();
+      const verbleibendeZeit = partyDatum - aktuelleZeit;
+
+      if (verbleibendeZeit <= 0) {
+        this.countdown = 'Die Party hat begonnen';
+        this.partyStarted = true; 
+      }
+      else {
+        const gesamtSekunden = Math.floor(verbleibendeZeit / 1000);
+        // nebenrechnung : (60 minuten  x 60 sekunden) x 24 Stunden = 86400 Sekunden pro Tag 
+        this.tage = Math.floor(gesamtSekunden / 86400);
+        this.stunden = Math.floor((gesamtSekunden % 86400) / 3600);
+        this.minuten = Math.floor((gesamtSekunden % 3600) / 60);
+        this.sekunden = gesamtSekunden % 60;
+        this.partyStarted = false; 
+      }
+
+    }, 1000);
+  }
 }
